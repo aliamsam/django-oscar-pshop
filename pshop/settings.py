e9608617e240a182f75cdc3894dbc54c3fbb0b25
aliamsam <118284025+aliamsam@users.noreply.github.com>
2026-01-02 17:34:21 +0330

    Update settings.py

--- pshop/settings.py
+++ pshop/settings.py
@@ -16,15 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-import os
-
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-# این خط را اضافه کنید:
->>>>>>> 083f690 (Add staticfiles)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Quick-start development settings - unsuitable for production
