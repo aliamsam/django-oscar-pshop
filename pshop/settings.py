bdaa72862ac4001385407c8a355c6bc1a6ba43a3
aliamsam <118284025+aliamsam@users.noreply.github.com>
2026-01-02 15:58:46 +0330

    Update settings.py

--- pshop/settings.py
+++ pshop/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y3686g#v#7m337x4wjim&@j9d!0ltiq36u&5!u3%$0cz_+_1_x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = false
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
