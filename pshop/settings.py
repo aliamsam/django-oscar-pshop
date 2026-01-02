3423ac87f3a1a49ef3b50fd3cfeb68d404b8c597
aliamsam <118284025+aliamsam@users.noreply.github.com>
2026-01-02 21:06:12 +0330

    Update settings.py

--- pshop/settings.py
+++ pshop/settings.py
@@ -13,6 +13,11 @@ from oscar.defaults import *
 from pathlib import Path
 
 import os
+
+from django.conf.locale import LANG_INFO
+
+LANG_INFO['fa']['bidi'] = True
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -117,8 +122,8 @@ OSCAR_REQUIRED_ADDRESS_FIELDS = (
     'country',
 )
 
-OSCAR_DEFAULT_CURRENCY = 'USD'
-OSCAR_ALLOWED_CURRENCIES = ('USD',)
+OSCAR_DEFAULT_CURRENCY = 'تومان'
+OSCAR_ALLOWED_CURRENCIES = ('تومان',)
 
 OSCAR_DYNAMIC_CLASS_LOADER = 'oscar.core.loading.default_class_loader'
 OSCAR_SLUG_ALLOW_UNICODE = True
@@ -187,14 +192,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
 
 USE_TZ = True
 
+USE_L10N = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
